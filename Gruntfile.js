8a797952e14d0e5301432fce2dca8d944e172f36
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2014-07-05 17:04:32 +0200

    Added jshint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,19 +4,31 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
 
-    // Project configuration.
+    // contrib-clean plugin configuration.
     clean: {
       all: [
         'public'
       ]
-    }
+    },
+
+    // contrib-jshint plugin configuration.
+    jshint: {
+     files: [
+       'Gruntfile.js',
+       'js/**/*.js'
+     ],
+     options: {
+     }
+   }
+
   });
 
 
-  // Load the plugin that provides the "uglify" task.
+  // Load the plugin that provides the tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
-  grunt.registerTask('default', ['clean:all']);
+  grunt.registerTask('default', ['clean:all', 'jshint']);
 
 };
