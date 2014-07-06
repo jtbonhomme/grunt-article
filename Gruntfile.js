f17592d9b82535dc11c8522a70db586282b28df3
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2014-07-06 10:26:42 +0200

    Added watch and shell tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,41 @@
 module.exports = function(grunt) {
   'use strict';
 
+  // Shell logging function
+  function logShell(err, stdout, stderr, cb) {
+    if (err) {
+      grunt.log.error('Command failed on ' + new Date());
+    } else {
+      grunt.log.ok('Command executed on ' + new Date());
+    }
+    cb();
+  }
+
   // Project configuration.
   grunt.initConfig({
+
+    // custom variables configuration
+    dirs: {
+      app:     'app/',
+      js:      'app/js/',
+      css:     'app/css/',
+      public:  'public/'
+    },
+
+    files: {
+      all: '**/*',
+      js:  '**/*.js',
+      css: '**/*.css',
+      img: '**/*.{png,gif,jpg,jpeg}'
+    },
+
+    // contrib-watch plugin configuration.
+    watch: {
+      jshint: {
+        files: ['<%= dirs.js %><%= files.js %>', '<%= dirs.public %><%= files.js %>', 'Gruntfile.js'],
+        tasks: 'jshint'
+      }
+    },
 
     // contrib-clean plugin configuration.
     clean: {
@@ -19,16 +52,30 @@ module.exports = function(grunt) {
      ],
      options: {
      }
-   }
+   },
 
-  });
+   shell: {
+    options: {
+      failOnError: true,
+      callback: logShell,
+      stdout: true,
+      stderr: true
+    },
+    gitlog: {
+      command: 'git log'
+    }
+  }
+
+ });
 
 
   // Load the plugin that provides the tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shell');
 
   // Default task(s).
-  grunt.registerTask('default', ['clean:all', 'jshint']);
+  grunt.registerTask('default', ['clean:all', 'jshint', 'shell:gitlog', 'watch']);
 
 };
